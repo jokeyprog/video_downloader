23e13f8e79770b0f7509c2683dea0b68a40285bc
jokeyprog <igorblag243@gmail.com>
2025-12-19 19:23:48 +0300

    Update

--- README.md
+++ README.md
@@ -1,45 +1,113 @@
-### Python 3 application for downloading videos from YouTube, VK Video, RuTube
-#### Version: 1.0 (current)
+### Скрипт на python для скачивания видео с vk.com (и не только) (GIU) 
+#### Version: 1.8 (текущая)
 
-### How do I start using it?
+![Light screenshot](https://raw.githubusercontent.com/blyamur/VK-Video-Download/main/app_screen.jpg)
 
-1. Download the archive with the latest version and unpack it to any place convenient for you.
+##### UPD: August 28 2025 Version 1.8
+Добавлены опции для выбора:
+Случайное имя |
+Ограничить длину до 50 симв. |
+Отдельная папка для видео |
+Исправлены некоторые проблемы
 
-2. Run the file *video_downloader.py* ;
+##### UPD: August 19 2025 Version 1.7
+Добавлено контекстное меню для мыши, что дает возможность вставлять\вырезать из поля ввода используя мышь.
 
-3. In the window that opens, paste the link to the video in the input field and click *Скачать видео*;
+##### UPD: Jule 4 2025 Version 1.6
+Добавлен вывод в лог файл и в консоль (часть строк закомментированы), оставшиеся при необходимости можете отключить
+Горячие клавиши в русской раскладке теперь работают. В связи с тем, что это особенность Tkinker, может чудить.
+Добавлена принудительная обработка в Tkinker горячих клавиш в РУС раскладке:
+Копировать *Control-c* | 
+Вставить *Control-v*  | 
+Вырезать *Control-x*  | 
+Выделить *Control-a*
 
-4. If the download is successful, a notification will appear, and the video will be saved in the *downloads* folder next to the file.
+##### UPD: April 2 2024 Version 1.5
+Код переделан на использовании yt-dlp, скачивание открытых видео снова доступно. 
+Функция download_video запускается в отдельном потоке, что позволяет основному потоку выполнения программы обновлять состояние интерфейса. В процессе скачивания, текущая информация отображается в интерфейсе.
+
+##### UPD: January 01 2025 Version 1.5
+Скрипт вполне успешно скачивает с таких сервисов как: vk.com, rutube.ru, Видео Mail.Ru, youtube.com и etc.  Проверялось пользователями, подробнее [можно почитать в обсуждении](https://github.com/blyamur/VK-Video-Download/issues/2)
+
+#### Что это?
+
+Скрипт на python 3, с графическим интерфейсом, для скачивания видео с сервиса vk.com 
 
 
-### FAQ
+#### Как начать использовать?
 
-- The link is not inserted in the input field
-> You probably have the Russian keyboard layout enabled, switch to English.
+1. Скачайте архив с последней версией и распакуйте в любое удобное для вас место; 
 
-- When downloading from YouTube, ask for *ffmpeg*
-> We know about this and are looking for ways to make friends with *yt_dlp* and *ffmpeg*.
+2. Установите необходимые компоненты и зависимости если такая необходимость имеется;
 
-- The video does not download
-> The video is either closed to outsiders or has a format\source not supported by non-dlp
+3. Запустите файл (скрипт) *vk_video_download.py* ;
 
-- Where is the finished video downloaded?
-> To the downloads directory, located in the same place where the application is hosted video_downloader.exe
+4. В появившемся окне вставьте ссылку на видео в поле ввода и нажмите кнопку *Скачать видео*;
+
+5. В случае удачного скачивания, появится уведомление, а видео будет сохранено в папке downloads
+
+> Ссылка должна быть вида: *https://vk.com/video-100000000_100000000* 
+
+
+
+#### Возможные проблемы
+
+- Не вставляется ссылка в поле ввода
+> ~У вас скорее всего включена русская раскладка клавиатуры, переключите на английскую.~
+
+> Обновитесь до последней версии, проблема должна быть решена
+
+
+
+- При закрытии окна программы, консоль продолжает выполняться
+> Баг связанн с продолжающимися процессами в фоне, будет решен в перспективе
+
+- Приложение (скрипт) во время скачивание зависает
+> Это особенность работы, в это время в фоне идет скачивание, по окончанию скачивания скрипт сам отвиснет.
+
+- Видео не скачивается
+> Видео или закрыто для доступа посторонним или имеет формат\источник не поддерживаемый yt-dlp
+
+- У файлов после скачивания  формат .unknown_video
+> Переименуйте в .mp4? как правило этого достаточно, чтобы решить проблему
+
+- Куда скачивается готовое видео?
+> В директорию downloads, находящуюся  там же где размещается ваш скрипт vk_video_download.py
+
+- В unix системах возможна [ошибка](https://github.com/blyamur/VK-Video-Download/issues/4) связанная с иконкой
+> необходимо закомментировать строку 158: ```#root.iconbitmap('./theme/icon.ico')```
 
 ---
-#### The *theme* folder contains files with application themes. The app is also functional without it.
-> For styles to work, the folder must be located next to the application.
+#### Для работы вам понадобится только содержимое папки *theme* и файл *vk_video_download.py*
 
-#### If you downloaded the files from the repository
-> Install the dependencies
+*  **vk_video_download.py** - скрипт для скачивания видео с vk.com 
+
+*  **theme** - Папка с темой оформления (стили, иконка и пр.)
+
+*  **requirements.txt** - Зависимости
+
+
+Команда для установка необходимых компонентов
 
     pip install -r requirements.txt
     
-> Run the file *video_downloader.py*
+Или отдельная установка yt-dlp
+
+    python3 -m pip install -U yt-dlp
+    
+Команда на сборку exe файла в pyinstaller: 
+
+    pyinstaller vk_video_download.py --noconsole --onefile --icon=icon.ico
+
+Или можно воспользоваться [GUI for Pyinstaller based on Tkinker](https://github.com/blyamur/GUI-Pyinstaller-Pichuga)
+    
+---
+***Скрипт был протестирован только в Windows с использованием версии Python 3.10.2***
 
 
+#### Полезные ссылки:
 
-### Useful links:
+[GUI for Pyinstaller based on Tkinker](https://github.com/blyamur/GUI-Pyinstaller-Pichuga)
 
 [yt-dlp Installation](https://github.com/yt-dlp/yt-dlp/wiki/Installation)
 
@@ -47,6 +115,24 @@
 
 [Requests manual](https://github.com/psf/requests)
 
-[Tkinker theme: Azure](https://github.com/rdbende/Azure-ttk-theme) - [rdbende](https://github.com/rdbende/)
+[Tkinker theme: Sun-Valley](https://github.com/rdbende/Sun-Valley-ttk-theme) - [rdbende](https://github.com/rdbende/)
 
-[The source code](https://github.com/blyamur/VK-Video-Download)
+[Tkinker theme: Spring-Noon](https://github.com/blyamur/Spring-Noon-ttk-theme) - [blyamur](https://github.com/blyamur/)
+
+[Visit the author's blog](https://blog/mons.ws?p=4485)
+
+#### Copyrights and Licenses
+Not for commercial use.
+
+
+*Thanks for reading :heart_eyes_cat:*
+> Спасибо за чтение!
+
+
+#### Did you find this useful?! | Вы нашли это  полезным ?!
+
+Happy to hear that :) *If You want to help me, you can buy me a cup of coffee :coffee: ( [yoomoney](https://yoomoney.ru/to/41001158104834) or [ko-fi](https://ko-fi.com/monseg), [boosty.to](https://boosty.to/monseg) )* 
+
+> Рад это слышать :) Если вы хотите мне помочь, вы можете угостить меня чашечкой кофе 
+  
+© 2024 From Russia with ❤ 
---
+++ vk_video_download.py
@@ -0,0 +1,416 @@
+import threading
+import tkinter as tk
+from tkinter import ttk, messagebox, Menu, PhotoImage
+import webbrowser
+import yt_dlp
+import requests
+import logging
+import os
+import random
+import string
+from logging.handlers import RotatingFileHandler
+import datetime
+import re
+import sys
+
+# Настройка логирования
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(),
+        RotatingFileHandler('vk_video_download.log', maxBytes=10*1024*1024, backupCount=5)
+    ]
+)
+logger = logging.getLogger(__name__)
+
+currentVersion = '1.8'
+
+class App(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self)
+        for index in [0, 1, 2]:
+            self.columnconfigure(index=index, weight=1)
+            self.rowconfigure(index=index, weight=1)
+            
+        # Контекстное меню
+        self.entry_context_menu = Menu(self, tearoff=0)
+        self.entry_context_menu.add_command(label="Вырезать", command=self.cut_text)
+        self.entry_context_menu.add_command(label="Копировать", command=self.copy_text)
+        self.entry_context_menu.add_command(label="Вставить", command=self.paste_text)
+        self.entry_context_menu.add_separator()
+        self.entry_context_menu.add_command(label="Выделить всё", command=self.select_all)
+        
+        # Хранилище прогресса
+        self.download_progress = {}
+        self.has_activity = False
+        self.active_threads = []  # Список активных потоков
+
+        self.setup_widgets()
+
+    def setup_widgets(self):
+        self.widgets_frame = ttk.Frame(self, padding=(0, 5, 0, 0))
+        self.widgets_frame.grid(row=0, column=1, padx=10, pady=(5, 0), sticky="nsew")
+        self.widgets_frame.columnconfigure(index=0, weight=1)
+
+        self.label = ttk.Label(
+            self.widgets_frame,
+            text="Вставьте ссылку(и) на видео через запятую",
+            justify="center",
+            font=("-size", 15, "-weight", "bold"),
+        )
+        self.label.grid(row=0, column=0, padx=0, pady=15, sticky="n")
+
+        self.entry_nm = ttk.Entry(self.widgets_frame, font=("Calibri 22"))
+        self.entry_nm.insert(tk.END, str(''))
+        self.entry_nm.grid(row=1, column=0, columnspan=10, padx=(5, 5), ipadx=150, ipady=5, pady=(0, 0), sticky="ew")
+        self.entry_nm.bind('<Return>', self.on_enter_pressed)
+
+        # Горячие клавиши
+        self.entry_nm.bind('<Control-c>', self.copy_text)
+        self.entry_nm.bind('<Control-v>', self.paste_text)
+        self.entry_nm.bind('<Control-x>', self.cut_text)
+        self.entry_nm.bind('<Control-a>', self.select_all)
+        self.entry_nm.bind('<Control-KeyPress>', self.handle_control_key)
+        self.entry_nm.bind("<Button-3>", self.show_context_menu)
+
+        # Кнопка "Скачать видео"
+        self.bt_frame = ttk.Frame(self, padding=(0, 0, 0, 0))
+        self.bt_frame.grid(row=1, column=0, padx=10, pady=0, columnspan=10, sticky="n")
+
+        self.accentbutton = ttk.Button(
+            self.bt_frame, text="Скачать видео", style="Accent.TButton", command=self.get_directory_string
+        )
+        self.accentbutton.grid(row=0, column=0, columnspan=3, ipadx=30, padx=2, pady=(5, 0), sticky="n")
+
+        self.bt_frame.columnconfigure(0, weight=1)
+
+        # --- Статусная строка ---
+        self.status_label = ttk.Label(
+            self.widgets_frame,
+            text="Готов к загрузке",
+            justify="center",
+            font=("-size", 10, "-weight", "normal"),
+            wraplength=600,
+            anchor="center",
+            foreground="#aaaaaa"
+        )
+        self.status_label.grid(row=3, column=0, padx=20, pady=(10, 5), sticky="ew")
+
+        # Убрать подсказку через 3 сек
+        root.after(3000, self.clear_hint_if_no_activity)
+
+        # --- Чекбоксы ---
+        self.check_frame = ttk.Frame(self.widgets_frame)
+        self.check_frame.grid(row=2, column=0, padx=20, pady=(5, 5), sticky="w")
+
+        self.var_random_name = tk.StringVar(value='')
+        self.var_limit_length = tk.StringVar(value='')
+        self.var_folder = tk.StringVar(value='')
+
+        self.check_random = ttk.Checkbutton(
+            self.check_frame,
+            text="Случайное имя",
+            variable=self.var_random_name,
+            onvalue='random',
+            offvalue='',
+            style="Switch.TCheckbutton"
+        )
+        self.check_random.grid(row=0, column=0, padx=(0, 10), pady=0, sticky="w")
+
+        self.check_limit = ttk.Checkbutton(
+            self.check_frame,
+            text="Ограничить длину до 50 симв.",
+            variable=self.var_limit_length,
+            onvalue='limit',
+            offvalue='',
+            style="Switch.TCheckbutton"
+        )
+        self.check_limit.grid(row=0, column=1, padx=(0, 10), pady=0, sticky="w")
+
+        self.check_folder = ttk.Checkbutton(
+            self.check_frame,
+            text="Отдельная папка для видео",
+            variable=self.var_folder,
+            onvalue='folder',
+            offvalue='',
+            style="Switch.TCheckbutton"
+        )
+        self.check_folder.grid(row=0, column=2, padx=0, pady=0, sticky="w")
+
+        # --- Кнопки ---
+        self.copy_frame = ttk.Frame(self, padding=(0, 0, 0, 10))
+        self.copy_frame.grid(row=8, column=0, padx=10, pady=5, columnspan=10, sticky="s")
+
+        self.UrlButton = ttk.Button(self.copy_frame, text="About", style="Url.TButton", command=self.openweb)
+        self.UrlButton.grid(row=1, column=0, padx=20, pady=0, columnspan=2, sticky="n")
+
+        self.UrlButton = ttk.Button(
+            self.copy_frame, text="Vers.: " + currentVersion + " ", style="Url.TButton", command=self.checkUpdate
+        )
+        self.UrlButton.grid(row=1, column=4, padx=20, pady=0, columnspan=2, sticky="w")
+
+        self.UrlButton = ttk.Button(self.copy_frame, text="Change theme", style="Url.TButton", command=self.change_theme)
+        self.UrlButton.grid(row=1, column=7, padx=20, pady=0, columnspan=2, sticky="w")
+
+    def clear_hint_if_no_activity(self):
+        if not self.has_activity:
+            self.status_label.configure(text="")
+
+    def show_context_menu(self, event):
+        try:
+            self.entry_nm.focus_set()
+            self.entry_context_menu.tk_popup(event.x_root, event.y_root)
+        except Exception as e:
+            logger.error(f"Error showing context menu: {str(e)}")
+            self.set_status_error(f"Ошибка: {str(e)}")
+        finally:
+            self.entry_context_menu.grab_release()
+
+    def handle_control_key(self, event):
+        try:
+            keycode = event.keycode
+            if keycode == 67:  # Ctrl+C / С
+                self.copy_text(); return "break"
+            elif keycode == 86:  # Ctrl+V / В
+                self.paste_text(); return "break"
+            elif keycode == 88:  # Ctrl+X / Ч
+                self.cut_text(); return "break"
+            elif keycode == 65:  # Ctrl+A / Ф
+                self.select_all(); return "break"
+        except Exception as e:
+            self.set_status_error(f"Ошибка: {str(e)}")
+            return "break"
+
+    def copy_text(self, event=None):
+        try:
+            self.entry_nm.event_generate("<<Copy>>")
+            return "break"
+        except Exception as e:
+            self.set_status_error(f"Ошибка: {str(e)}")
+            return "break"
+
+    def paste_text(self, event=None):
+        try:
+            self.entry_nm.event_generate("<<Paste>>")
+            return "break"
+        except Exception as e:
+            self.set_status_error(f"Ошибка: {str(e)}")
+            return "break"
+
+    def cut_text(self, event=None):
+        try:
+            self.entry_nm.event_generate("<<Cut>>")
+            return "break"
+        except Exception as e:
+            self.set_status_error(f"Ошибка: {str(e)}")
+            return "break"
+
+    def select_all(self, event=None):
+        try:
+            self.entry_nm.event_generate("<<SelectAll>>")
+            return "break"
+        except Exception as e:
+            self.set_status_error(f"Ошибка: {str(e)}")
+            return "break"
+
+    def openweb(self):
+        try:
+            webbrowser.open_new_tab('https://github.com/jokeyprog/video_downloader')
+        except Exception as e:
+            self.set_status_error(f"Ошибка: {str(e)}")
+
+    # def donate(self):
+    #     try:
+    #         webbrowser.open_new_tab('https://ko-fi.com/monseg')
+    #     except Exception as e:
+    #         self.set_status_error(f"Ошибка: {str(e)}")
+
+    def checkUpdate(self, method='Button'):
+        try:
+            logger.info("Проверка обновлений")
+            github_page = requests.get('https://raw.githubusercontent.com/jokeyprog/video_downloader/main/README.md')
+            github_page_html = str(github_page.content).split()
+            version = None
+            for i in range(0, 9):
+                try:
+                    idx = github_page_html.index(f'1.{i}')
+                    version = github_page_html[idx]
+                    break
+                except ValueError:
+                    continue
+
+            if version and float(version) > float(currentVersion):
+                update = messagebox.askyesno("Обновление", f"Доступна версия {version}. Обновиться?")
+                if update:
+                    webbrowser.open_new_tab('https://github.com/jokeyprog/video_downloader')
+            elif method == 'Button':
+                messagebox.showinfo("Обновления", "Нет новых версий")
+        except Exception as e:
+            logger.error(f"Ошибка проверки: {e}")
+
+    def change_theme(self):
+        # NOTE: The theme's real name is azure-<mode>
+        try:
+            if root.tk.call("ttk::style", "theme", "use") == "azure-dark":
+                # Set light theme
+                root.tk.call("set_theme", "light")
+            else:
+                # Set dark theme
+                root.tk.call("set_theme", "dark")
+        except Exception as e:
+            self.status_label.configure(text="Не найдена папка с темами")
+            logging.error(e)
+
+    def get_directory_string(self):
+        try:
+            urls_input = self.entry_nm.get().strip()
+            if not urls_input:
+                self.set_status_error("Введите ссылку")
+                return
+
+            video_urls = [url.strip() for url in urls_input.split(',') if url.strip()]
+            if not video_urls:
+                self.set_status_error("Нет корректных ссылок")
+                return
+
+            video_urls = list(dict.fromkeys(video_urls))  # Убрать дубли
+
+            self.has_activity = True
+            self.entry_nm.delete(0, tk.END)
+
+            for idx, url in enumerate(video_urls, start=1):
+                t = threading.Thread(target=self.download_video, args=(url, idx))
+                t.daemon = True  # Важно: поток завершится при закрытии основного
+                t.start()
+                self.active_threads.append(t)  # Сохраняем ссылку
+
+        except Exception as e:
+            logger.error(f"Ошибка: {e}")
+            self.set_status_error(f"Ошибка: {str(e)}")
+
+    def make_progress_hook(self, thread_id):
+        def hook(d):
+            self.my_hook(d, thread_id)
+        return hook
+
+    def download_video(self, video_url, serial_number):
+        try:
+            os.makedirs('downloads', exist_ok=True)
+
+            timestr = datetime.datetime.now().strftime('%d%m%Y_%H%M%S_%f')[:-4]
+            random_suffix = ''.join(random.choices(string.ascii_letters + string.digits, k=4))
+
+            use_random_name = (self.var_random_name.get() == 'random')
+            limit_length = (self.var_limit_length.get() == 'limit')
+            use_folder = (self.var_folder.get() == 'folder')
+
+            if use_random_name:
+                filename_base = f"{timestr}_{random_suffix}"
+            else:
+                with yt_dlp.YoutubeDL({'quiet': True}) as ydl:
+                    info = ydl.extract_info(video_url, download=False)
+                    title = info.get('title', 'video').strip().replace('/', '_').replace('\\', '_')
+                if limit_length:
+                    title = title[:50]
+                filename_base = f"{title}_{timestr}_{random_suffix}"
+
+            if use_folder:
+                outtmpl = f'downloads/{filename_base}/video.mp4'
+            else:
+                outtmpl = f'downloads/{filename_base}.mp4'
+
+            thread_id = f"#{serial_number}"
+            self.has_activity = True
+
+            ydl_opts = {
+                'outtmpl': outtmpl,
+                'quiet': True,
+                'progress_hooks': [self.make_progress_hook(thread_id)]
+            }
+
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([video_url])
+
+            self.download_progress[thread_id] = "Готово ✅"
+            self.update_progress_display()
+
+        except Exception as e:
+            logger.error(f"[{serial_number}] Ошибка: {e}")
+            self.download_progress[f"#{serial_number}"] = "Ошибка ❌"
+            self.has_activity = True
+            self.update_progress_display()
+
+    def my_hook(self, d, thread_id):
+        try:
+            self.has_activity = True
+            if d['status'] == 'downloading':
+                raw_percent = d.get('_percent_str', '0%')
+                clean_percent = re.sub(r'\x1b\[[0-9;]*m', '', raw_percent)
+                clean_percent = clean_percent.replace(',', '.').strip()
+                match = re.search(r'(\d+\.?\d*)\s*%', clean_percent)
+                percent = match.group(1) + "%" if match else "0%"
+
+                self.download_progress[thread_id] = percent
+                self.update_progress_display()
+
+            elif d['status'] == 'finished':
+                self.download_progress[thread_id] = "Готово ✅"
+                self.update_progress_display()
+
+        except Exception as e:
+            logger.error(f"Ошибка в my_hook: {e}")
+
+    def update_progress_display(self):
+        line = " ".join([f"{tid} {pct}" for tid, pct in self.download_progress.items()])
+        color = "#000000" if self.has_activity else "#aaaaaa"
+        root.after(0, lambda: self.status_label.configure(text=line, foreground=color))
+
+    def set_status_error(self, msg):
+        self.has_activity = True
+        root.after(0, lambda: self.status_label.configure(text=msg, foreground="#d93025"))
+
+    def on_enter_pressed(self, event):
+        self.get_directory_string()
+
+
+if __name__ == "__main__":
+    try:
+        if not os.path.exists('theme/vk_theme.tcl'):
+            raise FileNotFoundError("Файл темы не найден: theme/vk_theme.tcl")
+        if not os.path.exists('theme/icon.ico'):
+            raise FileNotFoundError("Файл иконки не найден: theme/icon.ico")
+
+        root = tk.Tk()
+        w = root.winfo_screenwidth() // 2 - 200
+        h = root.winfo_screenheight() // 2 - 200
+        root.geometry(f'680x350+{w}+{h}')
+        root.resizable(False, False)
+        root.title("Скачать видео")
+        try:
+            icon = PhotoImage(file="theme/icon.png")
+            root.iconphoto(False, icon)
+            root.iconbitmap("theme/icon.ico")
+            root.tk.call("source", "theme/azure.tcl")
+            root.tk.call("set_theme", "dark")
+        except Exception as e:
+            logging.error(e)
+
+        app = App(root)
+        app.pack(fill="both", expand=True)
+        root.update()
+        logger.info("Приложение запущено")
+
+        # --- Перехват закрытия окна ---
+        def on_closing():
+            root.destroy()
+            sys.exit(0)  # Завершить программу полностью
+
+        root.protocol("WM_DELETE_WINDOW", on_closing)
+
+        root.mainloop()
+
+    except Exception as e:
+        logger.error(f"Ошибка запуска: {e}")
+        messagebox.showerror("Ошибка", f"Ошибка запуска: {e}")
+        sys.exit(1)
